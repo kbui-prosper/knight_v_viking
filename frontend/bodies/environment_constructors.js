4f2dfda0cc3013fb7f442a7473974a8f4bf9a412
Khai Bui <khaivubui@gmail.com>
2017-10-02 14:49:06 -0700

    Adjust environment bodies to take in a boundThickness

--- frontend/bodies/environment_constructors.js
+++ frontend/bodies/environment_constructors.js
@@ -1,23 +1,29 @@
 import { Bodies } from 'matter-js';
 import { worldWidth, worldHeight } from '../knight_v_viking';
 
+const boundThickness = 50;
+
 export const leftWall = () => Bodies.rectangle(
-  0, worldHeight / 2, 100, worldHeight,
+  boundThickness / 2, worldHeight / 2,
+  boundThickness, worldHeight,
   { isStatic: true }
 );
 
 export const rightWall = () => Bodies.rectangle(
-  worldWidth, worldHeight / 2, 100, worldHeight,
+  worldWidth - boundThickness / 2,
+  worldHeight / 2, boundThickness, worldHeight,
   { isStatic: true }
 );
 
 export const ceiling = () => Bodies.rectangle(
-  worldWidth / 2, 0, worldWidth, 100,
+  worldWidth / 2, boundThickness / 2,
+  worldWidth, boundThickness,
   { isStatic: true }
 );
 
 export const ground = () => Bodies.rectangle(
-  worldWidth / 2, worldHeight, worldWidth, 100,
+  worldWidth / 2, worldHeight - boundThickness / 2,
+  worldWidth, boundThickness,
   {
     isStatic: true,
     render: {
