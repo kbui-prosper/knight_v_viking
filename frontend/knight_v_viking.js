970d58ca3b2e381266fb76c234077694d841f945
Khai Bui <khaivubui@gmail.com>
2017-10-02 11:05:24 -0700

    render background and adjust view

--- frontend/knight_v_viking.js
+++ frontend/knight_v_viking.js
@@ -4,9 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const engine = Engine.create();
 
   const render = Render.create({
-      element: document.querySelector("body"),
+      element: document.querySelector(".canvas-wrapper"),
       engine,
-      options: { wireframeBackground: 'transparent' }
+      options: {
+        wireframeBackground: 'rgba(255, 0, 0, 0.3)',
+        width: 1200,
+        height: 600
+      }
   });
 
   const ground = Bodies.rectangle(400, 610, 800, 100, { isStatic: true });
