47403792851c7e53d6fdf4415529a1a92faab7fa
Khai Bui <khaivubui@gmail.com>
2017-10-03 08:50:05 -0700

    Add left and right idle images

--- frontend/bodies/characters_constructors.js
+++ frontend/bodies/characters_constructors.js
@@ -17,6 +17,7 @@ window.viking.keyMap = {
 class BaseCharacter {
   constructor (charType) {
     this.charType = charType;
+    this.faceDirection = 'left';
     this.body = Bodies.rectangle(
       Math.random() * 800 + 100, 300,
       50, 90,
@@ -25,7 +26,7 @@ class BaseCharacter {
         render: {
           lineWidth: 5,
           sprite: {
-            texture: charType.idlePNGs[0],
+            texture: charType.idlePNGs[this.faceDirection][0],
             xScale: 0.1,
             yScale: 0.1,
             xOffset: -0.2,
@@ -45,11 +46,12 @@ class BaseCharacter {
   }
 
   nextIdle () {
+    const { idlePNGs } = this.charType;
     this.idlePNGsIndex =
-    (this.idlePNGsIndex + 1) % this.charType.idlePNGs.length;
+    (this.idlePNGsIndex + 1) % idlePNGs[this.faceDirection].length;
 
     this.body.render.sprite.texture =
-    this.charType.idlePNGs[this.idlePNGsIndex];
+    idlePNGs[this.faceDirection][this.idlePNGsIndex];
   }
 
   clearIdle () {
@@ -142,27 +144,3 @@ export class Viking extends BaseCharacter {
     super(window.viking);
   }
 }
-
-// const baseCharacter = (charType) => () => {
-//   const character = Bodies.rectangle(
-//     Math.random() * 400 + 100, 300,
-//     50, 90,
-//     {
-//       render: {
-//         lineWidth: 5,
-//         sprite: {
-//           texture: charType.idlePNGs[0],
-//           xScale: 0.1,
-//           yScale: 0.1,
-//           xOffset: -0.2,
-//           yOffset: -0.02
-//         }
-//       }
-//     }
-//   );
-//
-//   return character;
-// };
-//
-// export const knight = baseCharacter(window.knight);
-// export const viking = baseCharacter(window.viking);
