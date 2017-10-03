8afc18cabf4f0a249210b56acd9b8024ebf32b12
Khai Bui <khaivubui@gmail.com>
2017-10-02 17:28:45 -0700

    Comment out code to prepare for class refactoring

--- frontend/bodies/characters_constructors.js
+++ frontend/bodies/characters_constructors.js
@@ -1,25 +1,25 @@
 import { Bodies } from 'matter-js';
 
-const baseCharacter = (charType) => () => {
-  const character = Bodies.rectangle(
-    Math.random() * 400 + 100, 300,
-    50, 90,
-    {
-      render: {
-        lineWidth: 5,
-        sprite: {
-          texture: charType.idlePNGs[0],
-          xScale: 0.1,
-          yScale: 0.1,
-          xOffset: -0.2,
-          yOffset: -0.02
-        }
-      }
-    }
-  );
-
-  return character;
-};
-
-export const knight = baseCharacter(window.knight);
-export const viking = baseCharacter(window.viking);
+// const baseCharacter = (charType) => () => {
+//   const character = Bodies.rectangle(
+//     Math.random() * 400 + 100, 300,
+//     50, 90,
+//     {
+//       render: {
+//         lineWidth: 5,
+//         sprite: {
+//           texture: charType.idlePNGs[0],
+//           xScale: 0.1,
+//           yScale: 0.1,
+//           xOffset: -0.2,
+//           yOffset: -0.02
+//         }
+//       }
+//     }
+//   );
+//
+//   return character;
+// };
+//
+// export const knight = baseCharacter(window.knight);
+// export const viking = baseCharacter(window.viking);
